ef508ba1a9485842d102ecd14499f9f12b91d025
matt monk <mmonk185@gmail.com>
2016-02-24 15:37:33 -0500

    added final classes without controllers

---
+++ FinishedAssignment4/Models/CourseRoster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinishedAssignment4.Models
+{
+    public class CourseRoster
+    {
+        [Key]
+        public int CourseRosterID { get; set; }
+
+        public string CourseName { get; set; }
+
+        public int NoOfHours { get; set; }
+    }
+}
---
+++ FinishedAssignment4/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinishedAssignment4.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        public string StudentFirstName { get; set; }
+
+        public string StudentLastName { get; set; }
+
+        public string StudentAddress { get; set; }
+    }
+}
